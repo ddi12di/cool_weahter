3a626c869a6d97c330a208f644da87ae0b38dabe
ddi12di <160022432+ddi12di@users.noreply.github.com>
2024-06-27 20:55:41 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv, find_dotenv
 
 if not find_dotenv():
     exit("Переменные окружения  не загружены т.к отсутствует файл(ы) .env")
+    #может кнопку расположить в другом месте?
 else:
 
     load_dotenv()
